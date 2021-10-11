b883a0841e645ece51dd8d01af64e88a3e9cf3de
andy5995 <andy400-dev@yahoo.com>
2021-10-10 23:26:44 -0500

    MapFilter.js: don't overwrite any existing map filters

---
+++ gui/maps/MapFilters_community_maps_2.js
@@ -0,0 +1,7 @@
+MapFilters.prototype.Filters.push(
+	{
+		"Name": "community",
+		"Title": translate("Community Maps"),
+		"Description": translate("map filter", "Maps made by the community and shared publicly."),
+		"Match": ["community"]
+	});
