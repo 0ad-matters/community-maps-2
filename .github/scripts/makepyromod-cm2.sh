d267edd4ddd112eff6e84c211a5c1f1c17bfc354
andy5995 <andy400-dev@yahoo.com>
2022-02-24 16:23:43 -0600

    makepyromod-cm2.sh:improve script

--- .github/scripts/makepyromod-cm2.sh
+++ .github/scripts/makepyromod-cm2.sh
@@ -2,16 +2,48 @@
 
 set -ev
 
-if test -z $1; then
-  echo "supply a version number"
+echo $USER
+
+if [ -z "${1}" ]; then
+  echo "Usage: $0 <ver> <destdir>"
   exit 1
+fi
+
+if [ -z "${2}" ]; then
+	echo "supply a destination folder"
+	exit 1
+elif [ ! -d "${2}" ]; then
+	echo "directory '${2}' doesn't exist"
+	exit 1
+fi
+
+DESTDIR="${2}"
+VER="${1}"
+OUTFILE="community-maps-2-${VER}"
+
+for ext in pyromod zip; do
+	if [ -e "${DESTDIR}"/"${OUTFILE}".$ext ]; then
+		rm "${DESTDIR}"/"${OUTFILE}".$ext
+	fi
+done
+
+if [ ! -e "$PWD/mod.json" ]; then
+	echo "mod.json not found in $PWD"
+	exit 1
 fi
 
 0ad -mod=community-maps-2  \
 	-archivebuild=$PWD \
-	-archivebuild-output=$HOME/community-maps-2-${1}.pyromod \
+	-archivebuild-output="${DESTDIR}"/"${OUTFILE}".pyromod \
 	-archivebuild-compress
 
-test -e $HOME/community-maps-2-${1}.pyromod
-rm $HOME/community-maps-2-${1}.pyromod
+# If the pyromod fails to build, 0ad returns 0, so do an extra check to make
+# sure the fail exists
+test -e "${DESTDIR}"/"${OUTFILE}".pyromod
 
+if [ "${VER}" != "test" ]; then
+	zip -d "${DESTDIR}/${OUTFILE}.pyromod" ".github*"
+	cp "${DESTDIR}/${OUTFILE}.pyromod" "${DESTDIR}/${OUTFILE}.zip"
+else
+	rm "${DESTDIR}"/"${OUTFILE}".pyromod
+fi
