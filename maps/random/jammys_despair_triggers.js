91d3b496274ba31c8aaf33ab0489d7986bee7f18
andy5995 <arch_stanton5995@protonmail.com>
2022-07-18 12:54:09 -0500

    maps/random/jammys_despair.json:disable docks

---
+++ maps/random/jammys_despair_triggers.js
@@ -0,0 +1,23 @@
+var disabledTemplates = (civ) => [
+	"structures/" + civ + "/dock",
+	"structures/brit/crannog", // brit island settlement
+];
+
+Trigger.prototype.InitSurvival = function()
+{
+	this.SetDisableTemplates();
+};
+
+Trigger.prototype.SetDisableTemplates = function()
+{
+	for (let i = 1; i < TriggerHelper.GetNumberOfPlayers(); ++i)
+	{
+		let cmpPlayer = QueryPlayerIDInterface(i);
+		cmpPlayer.SetDisabledTemplates(disabledTemplates(QueryPlayerIDInterface(i, IID_Identity).GetCiv()));
+	}
+};
+
+{
+	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
+	cmpTrigger.RegisterTrigger("OnInitGame", "InitSurvival",{"enabled": true});
+}
