6ff6f40ac8404d3f079939cc6708d9607844dbde
andy5995 <arch_stanton5995@protonmail.com>
2022-08-05 11:42:54 -0500

    disable market upgrades

--- maps/random/triggers_disabled_traders.js
+++ maps/random/triggers_disabled_traders.js
@@ -1,25 +1,27 @@
-
-
-var disabledTemplates = (civ) => [
-	"units/" + civ + "/support_trader",
-	"units/" + civ + "/ship_merchant",
-];
-
 Trigger.prototype.InitSurvival = function()
 {
-	this.SetDisableTemplates();
+  this.SetDisableTemplates();
 };
 
 Trigger.prototype.SetDisableTemplates = function()
 {
-	for (let i = 1; i < TriggerHelper.GetNumberOfPlayers(); ++i)
-	{
-		let cmpPlayer = QueryPlayerIDInterface(i);
-		cmpPlayer.SetDisabledTemplates(disabledTemplates(QueryPlayerIDInterface(i, IID_Identity).GetCiv()));
-	}
+  const disabledTemplates = (civ) => [
+    "units/" + civ + "/support_trader",
+    "units/" + civ + "/ship_merchant",
+  ];
+
+  for (let i = 1; i < TriggerHelper.GetNumberOfPlayers(); ++i)
+  {
+    let cmpPlayer = QueryPlayerIDInterface(i);
+    const civ = QueryPlayerIDInterface(i, IID_Identity).GetCiv();
+    cmpPlayer.SetDisabledTemplates(disabledTemplates(civ));
+
+    // Also disable market upgrades
+    cmpPlayer.SetDisabledTechnologies(["trader_health", "trade_gain_01", "trade_gain_02", "trade_commercial_treaty"]);
+  }
 };
 
 {
-	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
-	cmpTrigger.RegisterTrigger("OnInitGame", "InitSurvival",{"enabled": true});
+  let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
+  cmpTrigger.RegisterTrigger("OnInitGame", "InitSurvival",{"enabled": true});
 }
