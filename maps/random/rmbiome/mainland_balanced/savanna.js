4b62ec980a223823c9445589df4dc05983eeccd1
andy5995 <arch_stanton5995@protonmail.com>
2022-09-27 09:52:35 -0500

    remove maps that are already included in the feldmap mod
    (see https://wildfiregames.com/forum/topic/53880-feldmap-a26/)

--- maps/random/rmbiome/mainland_balanced/savanna.js
+++
@@ -1,9 +0,0 @@
-function setupBiome_savanna()
-{
-	g_Gaia.mainHuntableAnimal = pickRandom([
-		"gaia/fauna_wildebeest",
-		"gaia/fauna_zebra",
-		"gaia/fauna_giraffe",
-		"gaia/fauna_elephant_african_bush"
-	]);
-}
--- maps/random/rmgen/library~feldmap.js
+++
@@ -1,4 +0,0 @@
-function getSurroundingAreas(positions, radius = 35)
-{
-	return positions.map(pos => createArea(new DiskPlacer(radius, pos), null, null));
-}
